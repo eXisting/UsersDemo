0fc438801d8298bc35c806bfb5102cd4f66df7b5
Andrey Popazov <andrey.plagiatix@gmail.com>
2018-10-22 08:55:30 -0700

    Initial commit

---
+++ UsersDemo/UsersDemo/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  UsersDemo
+//
+//  Created by Andrey Popazov on 10/22/18.
+//  Copyright © 2018 Andrey Popazov. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
