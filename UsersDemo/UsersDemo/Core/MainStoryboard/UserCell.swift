29dcb6c78edaa2d8db77e4822fab25725e54b268
Andrey Popazov <andrey.plagiatix@gmail.com>
2018-10-24 10:54:16 -0700

    Small UI fixes

--- UsersDemo/UsersDemo/Core/MainStoryboard/UserCell.swift
+++ UsersDemo/UsersDemo/Core/MainStoryboard/UserCell.swift
@@ -31,24 +31,7 @@ class UserCell : UITableViewCell {
     
      // MARK: Functions
     
-    public func setCellData(profile: UIImageView?, phone: String?, fullName: String?) {
-        
-//        if (photo != nil)
-//        {
-//            photo.layer.borderWidth = 1
-//            photo.layer.masksToBounds = false
-//            photo.layer.borderColor = UIColor.black.cgColor
-//            photo.layer.cornerRadius = photo.frame.height/2
-//            photo.clipsToBounds = true
-//        }
-        
-//        if validation(profile, phone, fullName) {
-//            self.profilePreview = profile!
-//            self.phone = phone!
-//            self.fullName = fullName!
-//        } else {
-//            //log("Some of passed fields have not passed validation!")
-//        }
+    @IBAction func onDetailedClick(_ sender: Any) {
         
     }
     
---
+++ UsersDemo/UsersDemo/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  UsersDemo
+//
+//  Created by Andrey Popazov on 10/24/18.
+//  Copyright © 2018 Andrey Popazov. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+}
